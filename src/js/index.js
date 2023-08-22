f0317be56cc600d800979de913ab40a9206b538e
Mêitti Freitas <125710800+Meitti@users.noreply.github.com>
2023-08-21 22:42:52 -0400

    final de aula 21/08

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,16 @@ const ulLista = document.querySelector('#ul-lista')
 const displayNome = document.querySelector('#display-nome')
 const btnChamar = document.querySelector('#btn-chamar')
 const listaDeAtendimento = new Array()
+
+function incluir(){
+listaDeAtendimento.push(inputNome.value)
+listar()
+}
+
+function listar(){
+for (let nome of listaDeAtendimento) {
+    console.log(nome)
+}
+}
+
+btnIncluir.onclick = incluir
